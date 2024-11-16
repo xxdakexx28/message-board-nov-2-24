6ff1089feeed640323296494213e76c00a5457cf
xxdakexx28 <bajormi.2807@gmail.com>
2024-11-16 12:21:15 -0400

    model add

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class publication(models.Model): 
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title 
+             

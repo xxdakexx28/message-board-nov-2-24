130810af233a0d5e439d08b78e5f126124fe5fa2
xxdakexx28 <bajormi.2807@gmail.com>
2024-11-23 10:00:33 -0400

    show_tasks

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import publication
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = publication
+    template_name = "post_list.html"
